bd1a93ffc6208421aadc166adf8ce55e5178bb32
yms213 <dbalstjd789@daum.net>
2022-09-27 16:47:47 +0900

    메인이미지부분 추가

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const slides = document.querySelector('.main');
+const slideImg = document.querySelectorAll('.main img')
+
+slideWidth = 1920;
+slideMargin = 0;
+
+function move(){
+
+    let curIndex = 0;
+
+    setInterval(function(){
+        slides.style
+    })
+}
